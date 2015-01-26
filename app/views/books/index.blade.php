3b2141a4de2992480b29a6c6af9bacf091a41bdc
Antonio Rubio <gazapo23@gmail.com>
2015-01-26 07:45:44 +0100

    feat #4: Update made

--- app/views/books/index.blade.php
+++ app/views/books/index.blade.php
@@ -10,7 +10,9 @@
                 {{ Form::open(array('route' => array('books.destroy', $book->id), 'method' => 'delete')) }}
                     {{ Form::submit('delete') }}
                 {{ Form::close() }}
-
+                {{ Form::open(array('route' => array('books.edit', $book->id), 'method' => 'get')) }}
+                {{ Form::submit('edit') }}
+                {{ Form::close() }}
             </li>
         @endforeach
     </ul>
