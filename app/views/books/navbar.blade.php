bae3dacaaa23afe207a569873f5728a73aab74e7
Antonio Rubio <antonio.rubio@actualmed.com>
2015-01-30 19:22:21 +0100

    feat #9: Added username at navbar

--- app/views/books/navbar.blade.php
+++ app/views/books/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-inverse">
-    <div class="navbar-header">
-        {{ HTML::linkRoute('books.index', 'Book Alert', array(), array('class' => 'navbar-brand')) }}
-    </div>
     <ul class="nav navbar-nav">
         <li>{{ HTML::linkRoute('books.index', 'View all Books') }}</li>
         <li>{{ HTML::linkRoute('books.create', 'Create a Book') }}</li>
     </ul>
+    <div class="nav navbar-nav navbar-right">
+        <p class="navbar-text" style="padding-right: 2em;">Welcome {{ Auth::user()->username }}</p>
+    </div>
 </nav>
