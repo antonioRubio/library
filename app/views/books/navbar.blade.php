ef6ba1f5fc6550908adb308599fa8e4aef62b20d
Antonio Rubio <gazapo23@gmail.com>
2015-01-31 08:17:23 +0100

    feat #10: logout button

--- app/views/books/navbar.blade.php
+++ app/views/books/navbar.blade.php
@@ -3,7 +3,8 @@
         <li>{{ HTML::linkRoute('books.index', 'View all Books') }}</li>
         <li>{{ HTML::linkRoute('books.create', 'Create a Book') }}</li>
     </ul>
-    <div class="nav navbar-nav navbar-right">
-        <p class="navbar-text" style="padding-right: 2em;">Welcome {{ Auth::user()->username }}</p>
-    </div>
+    <ul class="nav navbar-nav navbar-right" style="padding-right: 2em;">
+        <li class="navbar-text">Welcome {{ Auth::user()->username }}</li>
+        <li><button class="btn btn-danger navbar-btn"><a href="{{ URL::route('logout') }}" style="color: whitesmoke;"><span class="glyphicon glyphicon-off"></span></a></button></li>
+    </ul>
 </nav>
